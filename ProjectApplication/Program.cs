37b7c8fd4d49b63dd4838cb542a899bce4e213cf
Juan Manuel Guerrero Cañon <juanmanuel@nebula.com.co>
2023-05-22 18:44:14 -0500

    modify documentation

--- ProjectApplication/Program.cs
+++ ProjectApplication/Program.cs
@@ -5,7 +5,8 @@ using ProjectApplication.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
