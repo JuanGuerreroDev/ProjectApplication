cdbc0506897e6dabc050d9cc8560d6a75abcd443
Juan Manuel Guerrero Cañon <juanmanuel@nebula.com.co>
2023-05-22 18:41:48 -0500

    upgrade structure

---
+++ ProjectApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
